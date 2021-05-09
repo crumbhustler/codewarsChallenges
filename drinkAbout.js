e30071c0674b5a54b14e5dbcf805853ee06947d4
Ty Liddell <ty.liddell@gmail.com>
2021-05-09 17:22:36 -0500

    adding drink about js codewars challenge

---
+++ drinkAbout.js
@@ -0,0 +1,46 @@
+// Description:
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+//my solution
+// function peopleWithAgeDrink(old) {
+//   if (old < 14) {
+//     return "drink toddy";
+//   }
+//   if (old < 18) {
+//     return "drink coke";
+//   }
+//   if (old < 21) {
+//     return "drink beer";
+//   }
+//   if (old >= 21) {
+//     return "drink whisky";
+//   }
+// }
+
+//best practice
+const peopleWithAgeDrink = (age) =>
+    age < 14 ?
+    "drink toddy" :
+    age < 18 ?
+    "drink coke" :
+    age < 21 ?
+    "drink beer" :
+    "drink whisky";
