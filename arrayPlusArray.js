a7ec1bb70705f37aae9884fd3ca1b418eb9bef81
Ty Liddell <ty.liddell@gmail.com>
2021-05-15 18:06:58 -0500

    adding array plus array js codewars challenge

---
+++ arrayPlusArray.js
@@ -0,0 +1,15 @@
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+//my solution
+function arrayPlusArray(arr1, arr2) {
+    let one = arr1.concat(arr2);
+    let total = one.reduce((a, b) => a + b, 0);
+    return total; //something went wrong
+}
+
+//best practice
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
