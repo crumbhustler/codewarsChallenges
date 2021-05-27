8171cbea962bd92dac8a055deb64555bd030383d
Ty Liddell <ty.liddell@gmail.com>
2021-05-27 18:49:08 -0500

    adding convert a number to a string js codewars challenge

---
+++ convertANumberToAString.js
@@ -0,0 +1,15 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+//my solution
+function numberToString(num) {
+    return num.toString();
+}
+
+//arrow function
+const numberToString = (num) => String(num);
