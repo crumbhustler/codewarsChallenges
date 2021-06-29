d1e024fb00aebc5c9534c1550348382b0ff675bb
Ty Liddell <ty.liddell@gmail.com>
2021-06-29 10:19:16 -0500

    adding beginner reduce but grow js codewars challenge

---
+++ beginnerReduceButGrow.js
@@ -0,0 +1,15 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//my solution
+function grow(x) {
+    let sum = 1;
+    for (let i = 0; i < x.length; i++) {
+        sum = sum * x[i];
+    }
+    return sum;
+}
+
+//best practice
+const grow = (x) => x.reduce((a, b) => a * b);
