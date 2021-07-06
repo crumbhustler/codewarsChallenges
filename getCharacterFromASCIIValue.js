582ee6cdcb757079ec1b7f3f77a09c420e8db360
Ty Liddell <ty.liddell@gmail.com>
2021-07-06 11:19:11 -0500

    adding get character from ASCII value js codewars challenge

---
+++ getCharacterFromASCIIValue.js
@@ -0,0 +1,11 @@
+// Description:
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65) # => 'A'
+
+function getChar(c) {
+    let answer = String.fromCharCode(c);
+    return answer;
+}
