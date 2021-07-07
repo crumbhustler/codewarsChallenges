412b82914d0a8be4569206755599173ab55987ea
Ty Liddell <ty.liddell@gmail.com>
2021-07-07 11:04:37 -0500

    adding super duper easy js codewars challenge

---
+++ superDuperEasy.js
@@ -0,0 +1,13 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+//my code
+function problem(x) {
+    if (typeof x === "string") {
+        return "Error";
+    } else {
+        return x * 50 + 6;
+    }
+}
+
+//best practice
+const problem = (x) => (typeof x === "string" ? "Error" : x * 50 + 6);
