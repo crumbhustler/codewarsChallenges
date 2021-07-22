5ad775ae2f490b84fdc91c2a623dc4d72d29f368
Ty Liddell <ty.liddell@gmail.com>
2021-07-22 15:46:51 -0500

    adding function 2 squareing an argument js codewars challenge

---
+++ functionTwoSquareingAnArgument.js
@@ -0,0 +1,10 @@
+// Description:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//basic
+var square = function (a) {
+  return a * a;
+};
+
+//advanced
+const square = (number) => number ** 2;
