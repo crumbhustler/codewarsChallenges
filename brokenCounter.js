77e650402aa2a08ebf26264fd2852ee019e6d1b0
Ty Liddell <ty.liddell@gmail.com>
2021-07-23 10:52:51 -0500

    adding broken counter js codewars challenge

---
+++ brokenCounter.js
@@ -0,0 +1,37 @@
+// Description:
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+class Counter {
+  constructor() {
+    this.value = 0;
+  }
+
+  increase() {
+    this.value++;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  reset() {
+    this.value = 0;
+  }
+}
+
+//another way
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+};
+
+Counter.prototype.getValue = function () {
+  return this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+};
