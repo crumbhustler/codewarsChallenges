3affe9a02341697b71f78d41ac192dfcf268bca5
Ty Liddell <ty.liddell@gmail.com>
2021-12-05 16:50:57 -0600

    adding volume of a cuboid js codewars challenge

---
+++ volumeOfACuboid.js
@@ -0,0 +1,12 @@
+// Description:
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+const Kata = {
+  getVolumeOfCuboid: (length, width, height) => length * width * height,
+};
+
+class Kata {
+  static getVolumeOfCuboid(l, w, h) {
+    return l * w * h;
+  }
+}
