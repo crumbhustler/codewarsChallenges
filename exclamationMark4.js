f8e4ed329b12c00962564f2322aea4ab5952066a
Ty Liddell <ty.liddell@gmail.com>
2022-02-09 14:30:44 -0600

    adding exclamation mark 4 js codewars challange

---
+++ exclamationMark4.js
@@ -0,0 +1,18 @@
+// Description:
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi!"
+// remove("Hi!!!") === "Hi!"
+// remove("!Hi") === "Hi!"
+// remove("!Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!"
+// remove("Hi") === "Hi!"
+
+//best practice
+const remove = s => s.replace(/!+/g, "")+"!";
+
+//other way
+function remove(s) {
+  return s.replace(/!+/g, '') + '!';
+}
