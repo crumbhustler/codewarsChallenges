18c95bfbd6e8d1c5117bbb0146af5c061347c88e
Ty Liddell <ty.liddell@gmail.com>
2022-02-23 14:26:34 -0600

    adding find nearest square number js codwars challenge

---
+++ findNearestSquareNumber.js
@@ -0,0 +1,8 @@
+// Description:
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+function nearestSq(n){
+    return Math.round(n**0.5)**2
+}
