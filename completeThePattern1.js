d54d330695917f971daac10d09a37072c8af2afe
Ty Liddell <ty.liddell@gmail.com>
2022-08-19 16:51:54 -0600

    adding complete the pattern 1 js codewars challenge

---
+++ completeThePattern1.js
@@ -0,0 +1,22 @@
+// Task:
+// You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+// Note:Returning the pattern is not the same as Printing the pattern.
+// Rules/Note:
+// If n < 1 then it should return "" i.e. empty string.
+// There are no whitespaces in the pattern.
+// Pattern:
+// 1
+// 22
+// 333
+// ....
+// .....
+// nnnnnn
+
+function pattern(n) {
+  var res = [], i;
+  for(i = 1; i <= n; i++) {
+    res.push(Array(i + 1).join(i));
+  }
+  return res.join('\n');
+}
