73edd3d4e12004b19d1766d46646f3ec62ae436b
Ty Liddell <ty.liddell@gmail.com>
2023-04-14 10:06:05 -0500

    adding break camel case

---
+++ breakCamelCase.js
@@ -0,0 +1,13 @@
+//Description
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// complete the function
+function solution(string) {
+    const stringArray = string.split(/(?=[A-Z])/)
+    return stringArray.join(' ')
+}
